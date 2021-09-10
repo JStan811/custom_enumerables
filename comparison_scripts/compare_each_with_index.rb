91bdcbb2c1a4083fa97e30b31820dbdb8da81981
Jake Standley <jakestandley95@gmail.com>
2021-09-10 15:19:44 -0500

    wrote comparison scripts for each and each_with_index and moved them to comparison_scripts folder

---
+++ comparison_scripts/compare_each_with_index.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'enumerable'
+
+puts 'each_with_index vs. my_each_with_index'
+numbers = [1, 2, 3, 4, 5]
+puts 'Original:'
+original = (numbers.each_with_index { |item, index| puts "#{item}, #{index}" })
+puts ''
+puts 'Mine:'
+mine = (numbers.my_each_with_index { |item, index| puts "#{item}, #{index}" })
+puts ''
+puts "Original return value: #{original}"
+puts "My return value: #{mine}"
---
+++ comparison_scripts/comparison_each.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'enumerable'
+
+puts 'each vs. my_each'
+numbers = [1, 2, 3, 4, 5]
+puts 'Original:'
+original = (numbers.each { |item| puts item })
+puts ''
+puts 'Mine:'
+mine = (numbers.my_each { |item| puts item })
+puts ''
+puts "Original return value: #{original}"
+puts "My return value: #{mine}"
