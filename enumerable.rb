dad22641601974dffabbdea3188ada37340b981d
Jake Standley <jakestandley95@gmail.com>
2021-09-10 15:19:19 -0500

    wrote first two my_ method: each and each_with_index

--- enumerable.rb
+++ enumerable.rb
@@ -2,4 +2,17 @@
 
 # adding new methods to the existing Enumerable module
 module Enumerable
+  def my_each
+    for i in 0..(self.length - 1)
+      yield(self[i])
+    end
+    self
+  end
+
+  def my_each_with_index
+    for i in 0..(self.length - 1)
+      yield(self[i], i)
+    end
+    self
+  end
 end
